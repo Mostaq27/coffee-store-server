fe5febdda0b81dcf41d0780005445e82c146f16d
Mostaq27 <mostaq99muzahid@gmail.com>
2023-10-16 13:15:12 +0600

    user creat and post database, read user from database

--- index.js
+++ index.js
@@ -99,6 +99,19 @@ async function run() {
         res.send(result);
        }); 
 
+    //    
+    app.patch('/user',async(req, res)=>{
+        const user = req.body;
+        const filter = {email: user.email}
+        const updateDoc = {
+            $set: {
+                lastLoggedAt: user.lastLoggedAt
+            }
+        }
+        const result =await userCollection.updateOne(filter,updateDoc);
+        res.send(result);
+    })
+
        app.delete('/user/:id', async (req, res) =>{
         const id = req.params.id;
         const query = { _id: new ObjectId(id)}
